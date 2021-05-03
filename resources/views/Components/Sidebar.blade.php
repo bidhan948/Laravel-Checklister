9b1d5e563281a8870feb4611e6c73115e81d1033
Bidhan <Bidhanbaniya789@gmail.com>
2021-05-03 13:27:46 +0545

    Making a page controller ,making is_admin middleware and authenticating admin and also adding one more column in a user table  using migration command

---
+++ resources/views/Components/Sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
+    <div class="c-sidebar-brand d-md-down-none">
+        <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+            <use xlink:href="{{ asset('assets/brand/coreui-pro.svg#full') }}"></use>
+        </svg>
+        <svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
+            <use xlink:href="assets/brand/coreui-pro.svg#signet"></use>
+        </svg>
+    </div>
+    <ul class="c-sidebar-nav">
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('home') }}">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
+                </svg> Dashboard<span class="badge badge-info">{{ Auth::user()->name }}</span></a></li>
+        <li class="c-sidebar-nav-title">Admin</li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('admin.pages.index') }}">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+                </svg> Pages</a></li>
+    </ul>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,36 +8,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- CoreUI CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('Coreui/Style.css')}}">
 </head>
 <body>
     <main class="c-app c-dark-theme c-no-layout-transition">
-        <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
-            <div class="c-sidebar-brand d-md-down-none">
-                <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-                    <use xlink:href="{{ asset('assets/brand/coreui-pro.svg#full') }}"></use>
-                </svg>
-                <svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
-                    <use xlink:href="assets/brand/coreui-pro.svg#signet"></use>
-                </svg>
-            </div>
-            <ul class="c-sidebar-nav">
-                <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="index.html">
-                        <svg class="c-sidebar-nav-icon">
-                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
-                        </svg> Dashboard<span class="badge badge-info">NEW</span></a></li>
-                <li class="c-sidebar-nav-title">Components</li>
-                <li class="c-sidebar-nav-dropdown"><a class="c-sidebar-nav-dropdown-toggle" href="#">
-                        <svg class="c-sidebar-nav-icon">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-                        </svg> Base</a>
-                    <ul class="c-sidebar-nav-dropdown-items">
-                        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/breadcrumb.html">
-                                Breadcrumb</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+        @include('Components.Sidebar')
         <div class="c-wrapper">
             <header class="c-header c-header-light c-header-fixed">
                 <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar"
@@ -56,9 +31,9 @@
                     </svg>
                 </button>
                 <ul class="c-header-nav mfs-auto">
-                    <li class="c-header-nav-item dropdown"><a class="c-header-nav-link" data-toggle="dropdown" href="#"
+                    <li class="c-header-nav-item dropdown mx-3"><a class="c-header-nav-link" data-toggle="dropdown" href="#"
                             role="button" aria-haspopup="true" aria-expanded="false">
-                            <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/6.jpg"
+                            <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/FOFH8654.jpg"
                                     alt="user@email.com"></div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right pt-0">
@@ -84,12 +59,6 @@
                             </a>
                         </div>
                     </li>
-                    <button class="c-header-toggler c-class-toggler mfe-md-3" type="button" data-target="#aside"
-                        data-class="c-sidebar-show">
-                        <svg class="c-icon c-icon-lg">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-applications-settings"></use>
-                        </svg>
-                    </button>
                 </ul>
             </header>
             <div class="c-body">
@@ -99,10 +68,8 @@
             </div>
             <!-- Optional JavaScript -->
             <!-- Popper.js first, then CoreUI JS -->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"
-                integrity="sha512-yUNtg0k40IvRQNR20bJ4oH6QeQ/mgs9Lsa6V+3qxTj58u2r+JiAYOhOW0o+ijuMmqCtCEg7LZRA+T4t84/ayVA=="
-                crossorigin="anonymous"></script>
-            <script src="https://unpkg.com/@popperjs/core@2"></script>
-            <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
+            <script src="{{asset('Core-js/scrollbar.js')}}"></script>
+            <script src="{{asset('Core-js/popper.js')}}"></script>
+            <script src="{{asset('Core-js/dist.js')}}"></script>
 </body>
 </html>
